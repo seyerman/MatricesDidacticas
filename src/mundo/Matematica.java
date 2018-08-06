0a5bd8d301cdf62d9084c8ebd99ab4658d73b50b
jhonatan Bellaiza <jhonatan_bellaiza@hotmail.com>
2018-08-06 11:06:47 -0500

    Added load method to Matematica class

--- src/mundo/Matematica.java
+++ src/mundo/Matematica.java
@@ -1,6 +1,9 @@
 package mundo;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Matematica {
 	public final static String NOMBRE_ULTIMA_MATRIZ = "datos/UltimaMatrizGuardada.txt";
@@ -52,6 +55,17 @@ public class Matematica {
 	public void guardar() throws IOException{
 	}
 	
-	public void cargar(){
+	public void cargar()throws FileNotFoundException, IOException{
+		Scanner lector = new Scanner(new FileReader(NOMBRE_ULTIMA_MATRIZ));
+		
+		int f = lector.nextInt();
+		int c = lector.nextInt();
+		matrizDidactica = new Casilla[f][c];
+		for(int i = 0; i<matrizDidactica.length; i++) {
+			for(int j =0; j <matrizDidactica[i].length ; j++) {
+				matrizDidactica[i][j] = new Casilla(lector.nextInt());
+			}
+		}
+		lector.close();
 	}
 }
